29183ae71d349ec33d6cbaba56496fdfc320f549
Rian <rianalvesleal@gmail.com>
2024-03-14 19:03:39 -0300

    modificando IA

--- main.py
+++ main.py
@@ -1,8 +1,49 @@
+import random
+import copy
+
 def printTabuleiro(tabuleiro):
     for i in range(3):
         print("|".join(tabuleiro[i]))
         if(i < 2):
             print("------")
+
+# def verificaGanhador(tabuleiro):
+#     # Linhas
+#     for i in range(3):
+#         if (tabuleiro[i][0] == tabuleiro[i][1] == tabuleiro[i][2] != " "):  # Linhas horizontais
+#             return tabuleiro[i][0]
+
+#     # Colunas
+#     for i in range(3):
+#         if (tabuleiro[0][i] == tabuleiro[1][i] == tabuleiro[2][i] != " "):  # Colunas verticais
+#             return tabuleiro[0][i]
+
+#     # Diagonais
+#     if (tabuleiro[0][0] == tabuleiro[1][1] == tabuleiro[2][2] != " "):  # Diagonal principal
+#         return tabuleiro[0][0]
+#     if (tabuleiro[0][2] == tabuleiro[1][1] == tabuleiro[2][0] != " "):  # Diagonal secundária
+#         return tabuleiro[0][2]
+
+#     # Verifica o formato de "L"
+#     if (tabuleiro[0][0] == tabuleiro[1][0] == tabuleiro[1][1] != " " and
+#             tabuleiro[0][2] == tabuleiro[1][2] == tabuleiro[2][2] != " "):  # Formato de "L" para X
+#         return tabuleiro[1][1]
+#     elif (tabuleiro[0][0] == tabuleiro[0][1] == tabuleiro[1][1] != " " and
+#           tabuleiro[2][0] == tabuleiro[2][1] == tabuleiro[2][2] != " "):  # Formato de "L" para X
+#         return tabuleiro[1][1]
+#     elif (tabuleiro[0][1] == tabuleiro[1][1] == tabuleiro[2][0] != " " and
+#           tabuleiro[0][0] == tabuleiro[0][1] == tabuleiro[1][0] != " "):  # Formato de "L" para O
+#         return tabuleiro[1][1]
+#     elif (tabuleiro[0][1] == tabuleiro[1][1] == tabuleiro[2][0] != " " and
+#           tabuleiro[1][0] == tabuleiro[1][1] == tabuleiro[2][1] != " "):  # Formato de "L" para O
+#         return tabuleiro[1][1]
+
+#     for i in range(3):
+#         for j in range(3):
+#             if tabuleiro[i][j] == " ":
+#                 return False
+
+#     return "EMPATE"   [i, j] not in bloqueados and 
 
 def verificaGanhador(tabuleiro):
     # linhas 
@@ -30,87 +71,105 @@ def verificaGanhador(tabuleiro):
 
     return "EMPATE"
 
-#Mini Max
-def acoes(tabuleiro):
+def acoes(tabuleiro, bloqueados):
     posicoes = []
     for i in range(3):
         for j in range(3):
-            if(tabuleiro[i][j]==" "):
+            if tabuleiro[i][j] == " ":
                 posicoes.append([i,j])
     return posicoes
 
-def minimax(tabuleiro, jogador,score):
+def fazerjogada(tabuleiro,posicao,jogador):
+    novoTabuleiro = copy.deepcopy(tabuleiro)
+    novoTabuleiro[posicao[0]][posicao[1]] = simbolo[jogador]
+    return novoTabuleiro
+
+def minimax(tabuleiro, jogador, score):
     ganhador = verificaGanhador(tabuleiro)
-    if(ganhador):
+    if ganhador:
         return score[ganhador]
-    jogadas = acoes(tabuleiro)
-    
-    if(jogador == 0):   #MAX
-        melhorValor = 0
+
+    jogadas = acoes(tabuleiro,bloqueados)
+
+    if jogador == 0:   # MAX
+        melhorValor = float('-inf')  # Inicialize com o valor mínimo possível
         for jogada in jogadas:
-            tabuleiro[jogada[0]][jogada[1]] = simbolo[jogador]
-            jogador = (jogador + 1)%2
-            valor = minimax(tabuleiro, jogador,score)
-            tabuleiro[jogada[0]][jogada[1]] = " "
-            if(valor>melhorValor):
-                melhorValor = valor
+            aux = fazerjogada(tabuleiro,jogada,jogador)
+            jogador = (jogador + 1) % 2
+            valor = minimax(aux, jogador, score)
+            melhorValor = max(melhorValor, valor)
         return melhorValor
-    else:               #MIN
-        melhorValor = 0
+    else:               # MIN
+        melhorValor = float('inf')  # Inicialize com o valor máximo possível
         for jogada in jogadas:
-            tabuleiro[jogada[0]][jogada[1]] = simbolo[jogador]
-            jogador = (jogador + 1)%2
-            valor = minimax(tabuleiro, jogador,score)
-            tabuleiro[jogada[0]][jogada[1]] = " "
-            if(valor>melhorValor):
-                melhorValor = valor
+            aux = fazerjogada(tabuleiro,jogada,jogador)
+            jogador = (jogador + 1) % 2
+            valor = minimax(aux, jogador, score)
+            melhorValor = min(melhorValor, valor)
         return melhorValor
 
-def bot(tabuleiro,jogador,simbolo):
-    jogadas = acoes(tabuleiro)
+def bot(tabuleiro, jogador, simbolo, bloqueados):
+    jogadas = acoes(tabuleiro, bloqueados)
     melhorValor = 0
     melhorMovimento = 0
-    score = {"EMPATE": 0,"X": 1,"O": -1}
+    score = {"EMPATE": 0, "X": 1, "O": -1}
     for jogada in jogadas:
-        tabuleiro[jogada[0]][jogada[1]] = simbolo[jogador]
-        jogador = (jogador + 1)%2
-        valor = minimax(tabuleiro, jogador,score)
-        tabuleiro[jogada[0]][jogada[1]] = " "
-        if(valor>melhorValor):
+        aux = fazerjogada(tabuleiro,jogada,jogador)
+        jogador = (jogador + 1) % 2
+        valor = minimax(aux, jogador, score)
+        if melhorValor == 0:
             melhorValor = valor
             melhorMovimento = jogada
+        elif(jogador==0):
+            if(valor > melhorValor):
+                melhorValor = valor
+                melhorMovimento = jogada
+        elif(jogador==1):
+            if(valor < melhorValor):
+                melhorValor = valor
+                melhorMovimento = jogada
+
             
     return melhorMovimento[0], melhorMovimento[1]
 
-if(__name__ == "__main__"):
-    tabuleiro = [       #define tabuleiro
+if __name__ == "__main__":
+    tabuleiro = [       # Define o tabuleiro
         [" "," ", " "],
         [" "," ", " "],
         [" "," ", " "],
     ]
-    simbolo = ["X","O"] #define o simbolo para o jogador
-    jogador = 0         #0 primeiro jogador, 1 segundo jogador
+    simbolo = ["X","O"] # Define o símbolo para o jogador
+    jogador = 1        # 0 para o primeiro jogador, 1 para o segundo jogador
     ganhador = verificaGanhador(tabuleiro)
     
-    while(not ganhador):
+    # Bloquear dois espaços aleatórios
+    bloqueados = []
+    # for _ in range(2):
+    #     i, j = random.randint(0, 2), random.randint(0, 2)
+    #     bloqueados.append([i, j])
+    
+    while not ganhador:
         printTabuleiro(tabuleiro)
-        if(jogador == 0):   #primeiro jogador
+        if jogador == 0:   # Primeiro jogador
+            i, j = bot(tabuleiro, jogador, simbolo, bloqueados)
+            print("\nPrimeiro Jogador")
+        else:               # Segundo jogador
             i = int(input("Linha: "))
             j = int(input("Coluna: "))
-            print("\nPrimeiro Jogador")
-        else:               #segundo jogador
-            i,j = bot(tabuleiro,jogador,simbolo)
             print("\nSegundo Jogador")
             
-        if(tabuleiro[i][j] == " "):
-            tabuleiro[i][j] = simbolo[jogador]
-            jogador = (jogador+1)%2
+        if [i, j] not in bloqueados:
+            if tabuleiro[i][j] == " ":
+                tabuleiro[i][j] = simbolo[jogador]
+                jogador = (jogador + 1) % 2
+            else:
+                print("Posição já está ocupada")
         else:
-            print("Posição já está ocupada")
-        
+            print("Esta posição está bloqueada.")
+
         ganhador = verificaGanhador(tabuleiro)
-    
+
     printTabuleiro(tabuleiro)
-    print("Resultado: ",ganhador,"\n")
-        
-        
+    print("Resultado: ", ganhador, "\n")
+
+
